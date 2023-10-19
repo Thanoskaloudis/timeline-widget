6ebe99acb3ce3ab83e964f2ea050928e6678c82b
Thanoskaloudis <8anos.kal@gmail.com>
2023-10-19 14:35:34 +0100

    update schema models and create a template timeline in db

---
+++ app/utils/models.ts
@@ -0,0 +1,6 @@
+import { Timeline } from "@prisma/client";
+
+export interface ITimelinesProps {
+  timelines: Timeline
+}
+
--- lib/widgets.ts
+++ lib/widgets.ts
@@ -1,11 +1,42 @@
-import prisma from './prisma'
+import prisma from './prisma';
 
 export async function getTimelines() {
   try {
-    const timeLines = await prisma.timeLine.findMany()
-    return { timeLines }
+    const timelines = await prisma.timeline.findMany({
+      include: {
+        points: true,
+      },
+    })
+    return { timelines }
   } catch (error) {
     return { error }
   }
 }
 
+export async function createTimeline() {
+  try {
+    const timeline = await prisma.timeline.create({ data: { color: '#f25b50',
+    points: {
+      createMany: {
+        data: [{
+          checked: false,
+          text: "lorem ipsum",
+          color: "#f25b50"
+        },
+        {
+          checked: false,
+          text: "lorem ipsum",
+          color: "#f25b50"
+        },
+        {
+          checked: false,
+          text: "lorem ipsum",
+          color: "#f25b50"
+        }]
+      },
+    }}})
+    return { timeline }
+  } catch (error) {
+    return { error }
+  }
+}
