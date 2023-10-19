3b560e2d69a21622e6b0fb2c164243be74004269
Thanoskaloudis <8anos.kal@gmail.com>
2023-10-19 20:08:16 +0100

    create a PointDescription component to hanlde text update

--- app/components/PointDescription.tsx
+++ app/components/PointDescription.tsx
@@ -1,9 +1,44 @@
+'use client'  
+import { useState } from 'react';
 import { IMilestoneProps } from '../utils/models'
+import { updateTimelineAction } from '../_actions';
 
 export const PointDescription = ({ point } :IMilestoneProps) => {
+  const [isHovered, setIsHovered] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState(point.text);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedText(e.target.value);
+  };
+
+  const handleInputBlur = () => {
+    console.log(editedText);
+    updateTimelineAction(point.id, { text: editedText});
+    setIsEditing(false);
+  };
+
   return (
-    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-      {point.text}
-    </h3>
+    <div
+      onMouseEnter={() => setIsHovered(false)}
+      onMouseLeave={() => setIsHovered(true)}
+    >
+      {isHovered && !isEditing ? (
+        <h3
+          className="text-lg mt-4 font-semibold text-gray-900 dark:text-white cursor-pointer"
+          onClick={() => setIsEditing(true)}
+        >
+          {editedText}
+        </h3>
+      ) : (
+        <input
+          type="text"
+          className="block mt-4 text-sm font-normal leading-none text-gray-600 dark:text-gray-500"
+          value={editedText}
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+        />
+      )}
+    </div>
   )
 }
